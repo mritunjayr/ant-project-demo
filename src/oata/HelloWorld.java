fb85f06e8ff713458ee106a6a3ecd5c7e646ab96
mritunjay <mritunjaykumar694@gmail.com>
2020-04-08 16:29:10 +0530

    lib jar added

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -1,7 +1,14 @@
 package oata;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class HelloWorld {
+    private static Logger logger =LogManager.getLogger(HelloWorld.class);
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        // ConfigurationFactory.newConfigurationBuilder().build();
+        logger.info("Hello World");     
     }
+
 }
---
+++ src/oata/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package oata;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+
+
+public class HelloWorldTest {
+
+    @Test
+    public void testNothing(){
+
+    }
+
+    
+    @Test
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+
+}
